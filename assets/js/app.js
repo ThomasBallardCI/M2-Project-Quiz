d51fa4dd088c55d2de63966def1f1c8ae5bad4a3
thomas ballard <thomasgeballard@gmail.com>
2023-05-19 14:56:34 +0100

    laid out ideas + created css JS files

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+What it needs to do
+
+Store Name, Image and info on 27 different inline skates with indiviual id combinations
+
+provide the user 1 specific skate recomendation based on the Users InputDeviceInfo
+
+ask 3 questions and store responses
+
+use question responses to pull the skate information from the stored array of skates
+
+stretch goals 
+Google maps page to closest skate shops using maps Api 
+
+
